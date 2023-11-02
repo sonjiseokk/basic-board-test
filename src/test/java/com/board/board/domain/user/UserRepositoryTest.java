b789f66095d0e9beffa75894cc5044194240b6dc
sonjiseokk <thswltjr11@gmail.com>
2023-11-02 23:12:19 +0900

    feat: UserRepository 생성 및 save 로직 테스트

---
+++ src/test/java/com/board/board/domain/user/UserRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.board.board.domain.user;
+
+import com.board.board.domain.image.Image;
+import com.board.board.domain.image.ImageRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+@DataJpaTest
+@Transactional(readOnly = true)
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ImageRepository imageRepository;
+    @Autowired
+    private TestEntityManager entityManager;
+    @Test
+    @DisplayName("유저 저장")
+    @Transactional
+    void 유저_저장() throws Exception {
+        //given
+
+        /**
+         * 일단 mock여도 이미지가 저장되어야 하므로
+         * 이미지 레포지토리를 사용하여 미리 저장
+         */
+        Image mockImage = mock(Image.class);
+        imageRepository.save(mockImage);
+
+        User user = User.builder()
+                .email("aaa@bbb.com")
+                .username("test")
+                .password("test2")
+                .profileImage(mockImage)
+                .build();
+        //when
+        /**
+         * user 저장 반영을 위해 flush
+         */
+        User savedUser = userRepository.save(user);
+        entityManager.flush();
+
+        /**
+         * testEntityManager를 통해 데이터베이스에서 가져오는 것 처럼 모방
+         */
+        User retrievedUser = entityManager.find(User.class, savedUser.getId());
+        //then
+        assertThat(retrievedUser).isNotNull();
+    }
+}
