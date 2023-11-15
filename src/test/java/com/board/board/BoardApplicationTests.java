a2b477da9907e9c44d6d0f70b4fd5567e69089f5
sonjiseokk <thswltjr11@gmail.com>
2023-11-15 16:28:20 +0900

    fix: build 테스트2

--- src/test/java/com/board/board/BoardApplicationTests.java
+++ src/test/java/com/board/board/BoardApplicationTests.java
@@ -1,9 +1,7 @@
 package com.board.board;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BoardApplicationTests {
 
 	@Test
